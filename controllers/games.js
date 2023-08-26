e76c4279d58f3d913bdab62a293fa1f86845c7cb
hWeitian <huangweitian1@hotmail.com>
2023-08-26 11:29:47 +0800

    Fix issue when no filter is selected and return all games

--- controllers/games.js
+++ controllers/games.js
@@ -10,11 +10,8 @@ const { Op, QueryTypes, Sequelize } = require("sequelize");
 const { Game, Gametag, Tag } = db;
 
 async function getSelectedGames(req, res) {
-  // console.log(req.query.data);
-  console.log("hello");
   const data = req.query.data;
   const { queryData, selectionType } = revertSelection(data);
-  // console.log(queryData);
   let games;
 
   try {
@@ -35,44 +32,32 @@ async function getSelectedGames(req, res) {
     // console.log("required play style", requiredTagId);
 
     const excludedTagId = generateExcludedTagId(queryData, finalTags);
-    // console.log("excluded tag", excludedTagId);
-
-    // const games = await Game.findAll({
-    //   include: [
-    //     {
-    //       model: Tag,
-    //       through: {
-    //         model: Gametag,
-    //         attributes: [],
-    //       },
-    //       // where: { id: requiredTagId },
-    //       // where: {
-    //       //   id: { [Op.notIn]: [4] },
-    //       // },
-    //       // where: {
-    //       //   id: {
-    //       //     [Op.in]: [1, 2, 3],
-    //       //     [Op.notIn]: 4,
-    //       //   },
-    //       // },
-    //       where: {
-    //         [Op.and]: [{ id: requiredTagId }, { id: { [Op.notIn]: [4] } }],
-    //       },
-    //       // where: {
-    //       //   [Op.and]: [{ id: requiredTagId }, { [Op.notIn]: { id: 4 } }],
-    //       // },
-    //     },
-    //   ],
-    //   // where: {
-    //   //   "$Tags.id$": { [Op.in]: requiredTagId }, // Exclude games with tag ID 4
-    //   // },
-    //   where: {
-    //     "$Tags.id$": { [Op.ne]: 4 }, // Exclude games with tag ID 4
-    //   },
-    //   required: false,
-    // });
-
-    if (selectionType === "playWithout") {
+    // If no tags are selected, return all games
+    if (selectionType === "all") {
+      games = await sequelize.query(
+        `SELECT
+      g.id AS "id",
+      g.title AS "title",
+      g.content AS "content",
+      JSON_AGG(
+        JSON_BUILD_OBJECT(
+          'id', t.id,
+          'tag', t.tag,
+          'createdAt', t.created_at,
+          'updatedAt', t.updated_at
+        )
+      ) AS "Tags"
+    FROM games AS g
+    JOIN gametags AS gt ON g.id = gt.game_id
+    JOIN tags AS t ON gt.tag_id = t.id
+    GROUP BY g.id
+    ORDER BY g.id;`,
+        {
+          model: Game,
+          mapToModel: true, // pass true here if you have any mapped fields
+        }
+      );
+    } else if (selectionType === "playWithout") {
       // If game contains any of the excluded tag, it will not be included in final result
       games = await sequelize.query(
         `SELECT
@@ -132,8 +117,6 @@ async function getSelectedGames(req, res) {
         }
       );
     }
-
-    // console.log(games);
 
     return res.json(games);
   } catch (err) {
